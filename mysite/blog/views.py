60ffc6a1eae1ace8b287162c55e4c2a1d222d2e5
FreeSpaceMan <sar.bartkus@gmail.com>
2020-09-20 23:31:39 +0300

    second commit

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -18,7 +18,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date')) #this has to be fixed
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #this has to be fixed
 
 class PostDetailView(DetailView):
     model = Post
@@ -38,3 +38,11 @@ class PostUpdateView(LoginRequiredMixin,UpdateView):
 class PostDeleteView(LoginRequiredMixin,DeleteView):
     model = Post
     success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url='/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objecrs.filter(published_date_isnull=True).order_by('created_date')
