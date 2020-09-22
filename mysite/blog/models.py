fef25c45658e814836974542b060f98b7f4b2283
FreeSpaceMan <sar.bartkus@gmail.com>
2020-09-23 02:12:14 +0300

    third commit

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=256)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
@@ -25,8 +25,8 @@ class Post(models.Model):
         return self.title
 
 
-class Comment(model.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.DO_NOTHING)
     author = models.CharField(max_length=256)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
